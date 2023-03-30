ad3a70e6551b8173de96ba32b630d2c2aed96075
YE nicolas <nicolas.ye@etu.u-paris.fr>
2023-03-30 11:56:44 +0200

    beazout seems to work, have to make it iterative

--- Polynomial.py
+++ Polynomial.py
@@ -39,7 +39,6 @@ class Polynomial :
             if (self.coeff[i] != 0) : break
             self.coeff = self.coeff[:i]
             self.deg -= 1
-         
 
     # Getter du coeff de X^i
     def get(self, i) : 
@@ -72,9 +71,10 @@ class Polynomial :
 
     # Soustraction
     def sub(self, p) :
-        for i in range(len(p.coeff)) :
-            p.coeff[i] = -p.coeff[i]
-        return self.add(p)
+        s = p.copy()
+        for i in range(len(s.coeff)) :
+            s.coeff[i] = -s.coeff[i]
+        return self.add(s)
 
     # Multiplication
     def mul(self, p) :
@@ -84,7 +84,6 @@ class Polynomial :
             for j in range(len(p.coeff)) :
                 p4.coeff[i + j] = self.coeff[i] * p.coeff[j]
             result = result.add(p4)
-        result.trim()
         return result
 
     # Euclidian div par un polynome unitaire
@@ -104,41 +103,22 @@ class Polynomial :
 
         return (q, r)
 
-    # Bezout
-    def extendedGCD(self, p):
-        r0 = self.copy() 
-        r1 = p.copy()
-        s0 = Polynomial(0, fixed_value = [1])
-        s1 = Polynomial(0, fixed_value = [0]) 
-        t0 = Polynomial(0, fixed_value = [0])
-        t1 = Polynomial(0, fixed_value = [1])
-
-        i = 1
-        while (r1.coeff[0] != 0 or r1.deg != 0) :
-            print("r1 =", r1)
-            print("deg r1 =", r1.deg)
-            q = r0.euclidianDiv(r1)[0]
-            qri = q.mul(r1)
-            qsi = q.mul(s1)
-            qti = q.mul(t1)
-
-            r2 = r0.sub(qri)
-            s2 = s0.sub(qsi)
-            t2 = t0.sub(qti)
-
-            r0 = r1
-            r1 = r2
-            s0 = s1
-            s1 = s2
-            t0 = t1
-            t1 = t2
-
-            i += 1
-        return r1
+    def bezout(self, p) :
+        if self.isZero() : return p, Polynomial(0, fixed_value = [0]), Polynomial(0, fixed_value = [1])
+                
+        q,r = p.euclidianDiv(self)
+        gcd,x1,y1 = r.bezout(self)
+        
+        # Update x and y using results of recursive
+        # call
+        x = y1.sub(q.mul(x1))
+        y = x1
+        
+        return gcd,x,y
 
 # Tests
 if __name__ == "__main__" :
-    p1 = Polynomial(2, fixed_value= [2,-1,-1])
+    p1 = Polynomial(4, fixed_value = [1,1,1,1])
     p2 = Polynomial(2, fixed_value = [1,1,1])
     print("P1 =", p1)
     print("P2 =", p2)
@@ -147,4 +127,5 @@ if __name__ == "__main__" :
     print("P1 * P2 =", p1.mul(p2))
     q,r = p1.euclidianDiv(p2)
     print("P1 = P2*Q + R = (", p2, ") * (", q, ") + (", r, ") =", (p2.mul(q)).add(r))
-    print("Bezout : ", p1.extendedGCD(p2))
+    r,u,v = p1.bezout(p2)
+    print("R = P1*U + P2*V = (", p1, ")*(", u, ") + (", p2, ")*(", v, ") =", (p1.mul(u)).add(p2.mul(v)))
